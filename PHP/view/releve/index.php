e42565bbd4dadce14a9aeb1e54784810a719c29a
CAULLIREAU Dorian <54810120+caullird@users.noreply.github.com>
2022-01-12 14:17:32 +0100

    relevé de note

--- PHP/view/releve/index.php
+++ PHP/view/releve/index.php
@@ -16,28 +16,38 @@
         </div>
     </div>
 </div>
-<div class="widget-program-bg">
+
+<div class="static-table-area">
     <div class="container-fluid">
-        <?php
-           foreach($data['educationalunit'] as $educationalunit){
-               echo "////" . $educationalunit->title_educationalunit;
-               echo "<br>";
-               foreach($data['module_unit'] as $moduleunit){
-                   if($moduleunit->ideducationalunit->ideducationalunit == $educationalunit->ideducationalunit){
-                       echo "///////////" . $moduleunit->idmodule->title_module;
-                       echo "<br>";
-                       foreach($data['evaluation'] as $evaluation){
-                           if(($evaluation->idmodule->idmodule == $moduleunit->idmodule->idmodule)){
-                               echo $evaluation->title_evaluation." | ".$evaluation->mark_evaluation." | ".$evaluation->comment_evaluation;
-                           }
-                       }
-                   }
-               }
-
-        ?>
-
-        <?php
-            }
-        ?>    
+        <div class="row">
+            <div class="col-lg-12 col-md-6 col-sm-6 col-xs-12">
+                <div class="sparkline8-list">
+                        <div class="static-table-list">
+                            <table class="table">
+                                <tbody>
+                                <?php
+                                foreach($data['educationalunit'] as $educationalunit){
+                                    echo "<tr>";
+                                    echo "<td><b>" . $educationalunit->title_educationalunit . "</b></td>";
+                                    foreach($data['module_unit'] as $moduleunit){
+                                        if($moduleunit->ideducationalunit->ideducationalunit == $educationalunit->ideducationalunit){
+                                            echo "<tr>";
+                                            echo "<td>&ensp;&ensp;" . $moduleunit->idmodule->title_module . "</td>";
+                                            echo "<tr>";
+                                            foreach($data['evaluation'] as $evaluation){
+                                                if(($evaluation->idmodule->idmodule == $moduleunit->idmodule->idmodule)){
+                                                    echo "<td>&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;" . $evaluation->title_evaluation ." | ".$evaluation->comment_evaluation . " | Note : <b>" . $evaluation->mark_evaluation . "</b></td>";
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+            </div>
+        </div>
     </div>
 </div>
